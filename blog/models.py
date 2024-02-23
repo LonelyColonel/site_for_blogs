3831bbb5e31fa796d52cf67bbdbb687a8e4bfe6b
Lonely_Colonel <lonely_colonel@mail.ru>
2024-02-24 02:23:43 +0300

    views, licence, pagination

--- blog/models.py
+++ blog/models.py
@@ -20,7 +20,7 @@ class Post(models.Model):
     published = PublishedManager()
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
                                related_name='blog_posts')
@@ -44,4 +44,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse('blog:post_detail', args=[self.publish.year, 
+                                                 self.publish.month, 
+                                                 self.publish.day, 
+                                                 self.slug])
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post_slug):
+    post = get_object_or_404(Post, slug=post_slug, publish__year=year, 
+                             publish__month=month, publish__day=day, 
+                             status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
